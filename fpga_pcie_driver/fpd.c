d1f4c907627946ea1e6275452722aa3f9c973186
Yangyu Chen <cyy@cyyself.name>
2023-04-01 19:15:56 +0800

    msi ok

--- fpga_pcie_driver/fpd.c
+++ fpga_pcie_driver/fpd.c
@@ -2,12 +2,16 @@
 #include <linux/kernel.h>
 #include <linux/pci.h>
 #include <linux/dma-mapping.h>
+#include <linux/interrupt.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Yangyu Chen (cyy@cyyself.name)");
 MODULE_DESCRIPTION("Driver for FPGA PCIe Demo");
 
 //#define REQ_REGION
+#define REQ_MSI
+#define FPD_NR_IRQS 1
+
 const size_t fpd_dma_size = 1024ul*1024;
 
 static const struct pci_device_id fpd_pci_ids[] = {
@@ -20,6 +24,7 @@ struct fpd_driver_obj {
 	bool dma_enabled;
 	dma_addr_t device_dma_addr;
 	void *cpu_addr;
+	int irq_num[FPD_NR_IRQS];
 };
 
 int fpd_setup_dma(struct pci_dev *dev, struct fpd_driver_obj *driver_obj) {
@@ -44,9 +49,17 @@ err:
 	return ret;
 }
 
+static irqreturn_t fpd_irq_handler(int irq, void *arg) {
+	pr_info("Got irq from number %d\n", irq);
+	return IRQ_HANDLED;
+}
+
 static int fpd_pci_probe(struct pci_dev *dev, const struct pci_device_id *id) {
 	struct fpd_driver_obj *driver_obj;
+	unsigned int i, j;
 	int ret;
+
+	driver_obj = kzalloc(sizeof(struct fpd_driver_obj), GFP_KERNEL);
 
 	ret = pci_enable_device(dev);
 	if (ret) {
@@ -58,37 +71,72 @@ static int fpd_pci_probe(struct pci_dev *dev, const struct pci_device_id *id) {
 	ret = pci_request_regions(dev, KBUILD_MODNAME);
 	if (ret) {
 		dev_err(&dev->dev, "pci_request_regions FAILED\n");
+		pci_disable_device(dev);
 		goto err;
 	}
 #endif
-	ret = pci_enable_msi(dev);
-	if (ret) {
-		dev_err(&dev->dev, "pci_enable_msi FAILED\n");
-		goto err;
+
+#ifdef REQ_MSI
+	ret = pci_alloc_irq_vectors(dev, FPD_NR_IRQS, FPD_NR_IRQS, PCI_IRQ_MSI | PCI_IRQ_MSIX);
+	if (ret < 0) {
+		dev_err(&dev->dev, "Unable to allocate irq vectors\n");
+		goto err_free_region;
 	}
 
-	driver_obj = kzalloc(sizeof(struct fpd_driver_obj), GFP_KERNEL);
+	for (i = 0; i < FPD_NR_IRQS; ++ i) {
+		driver_obj->irq_num[i] = pci_irq_vector(dev, i);
+		if (driver_obj->irq_num[i] < 0) {
+			dev_err(&dev->dev, "Unable to get Linux irq number\n");
+			goto err_free_irq;
+		}
+		ret = request_irq(driver_obj->irq_num[i], fpd_irq_handler, IRQF_SHARED, "fpd_irq", &dev->dev);
+		if (ret) {
+			dev_err(&dev->dev, "Unable to request irq\n");
+			goto err_free_irq;
+		}
+	}
+#endif
 	dev_set_drvdata(&dev->dev, driver_obj);
 	fpd_setup_dma(dev, driver_obj);
 
 	dev_info(&dev->dev, "init ok!\n");
 	return 0;
+err_free_irq:
+	for (j = 0; j < i; ++ j) {
+		free_irq(driver_obj->irq_num[j], &dev->dev);
+	}
+	pci_free_irq_vectors(dev);
+err_free_region:
+#ifdef REQ_REGION
+	pci_release_regions(dev);
+#endif
 err:
+	pci_disable_device(dev);
+	kfree(driver_obj);
 	return ret;
 }
 
 void fpd_pci_remove(struct pci_dev *dev) {
 	struct fpd_driver_obj *driver_obj;
+	unsigned int i;
+
 	driver_obj = dev_get_drvdata(&dev->dev);
-	pci_disable_device(dev);
+	if (driver_obj == NULL) return;
 #ifdef REQ_REGION
 	pci_release_regions(dev);
 #endif
-	pci_disable_msi(dev);
+#ifdef REQ_MSI
+	for (i = 0; i < FPD_NR_IRQS; ++ i) {
+		free_irq(driver_obj->irq_num[i], &dev->dev);
+	}
+	pci_free_irq_vectors(dev);
+#endif
 	if (driver_obj->dma_enabled) {
 		pci_clear_master(dev);
 		dma_free_coherent(&dev->dev, fpd_dma_size, driver_obj->cpu_addr, driver_obj->device_dma_addr);
 	}
+	pci_disable_device(dev);
+	kfree(driver_obj);
 }
 
 static struct pci_driver fpd_driver = {
